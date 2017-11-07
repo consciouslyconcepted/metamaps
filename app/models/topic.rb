7196b94363b4eb5a5b987fef78b64c29bc2087b8
Devin Howard <devin@callysto.com>
2017-11-06 19:42:39 -0800

    manual rubocop fixes

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -127,14 +127,9 @@ class Topic < ApplicationRecord
         end
       end
     end
-    if output_format == 'array'
-      return output
-    elsif output_format == 'text'
-      return output.join('; ')
-    else
-      raise 'invalid argument to synapses_csv'
-    end
-    output
+    return output if output_format == 'array'
+    return output.join('; ') if output_format == 'text'
+    raise 'invalid argument to synapses_csv'
   end
 
   def topic_autocomplete_method
@@ -144,7 +139,7 @@ class Topic < ApplicationRecord
   protected
 
   def set_perm_by_defer
-    permission = defer_to_map.permission if defer_to_map
+    self.permission = defer_to_map.permission if defer_to_map
   end
 
   def create_metamap?
@@ -163,22 +158,22 @@ class Topic < ApplicationRecord
   handle_asynchronously :after_created_async
 
   def after_updated
-    if ATTRS_TO_WATCH.any? { |k| changed_attributes.key?(k) }
-      new = attributes.select { |k| ATTRS_TO_WATCH.include?(k) }
-      old = changed_attributes.select { |k| ATTRS_TO_WATCH.include?(k) }
-      meta = new.merge(old) # we are prioritizing the old values, keeping them
-      meta['changed'] = changed_attributes.keys.select { |k| ATTRS_TO_WATCH.include?(k) }
-      Events::TopicUpdated.publish!(self, updated_by, meta)
-      maps.each do |map|
-        ActionCable.server.broadcast 'map_' + map.id.to_s, type: 'topicUpdated', id: id
-      end
+    return unless ATTRS_TO_WATCH.any? { |k| changed_attributes.key?(k) }
+
+    new = attributes.select { |k| ATTRS_TO_WATCH.include?(k) }
+    old = changed_attributes.select { |k| ATTRS_TO_WATCH.include?(k) }
+    meta = new.merge(old) # we are prioritizing the old values, keeping them
+    meta['changed'] = changed_attributes.keys.select { |k| ATTRS_TO_WATCH.include?(k) }
+    Events::TopicUpdated.publish!(self, updated_by, meta)
+    maps.each do |map|
+      ActionCable.server.broadcast 'map_' + map.id.to_s, type: 'topicUpdated', id: id
     end
   end
 
   def after_updated_async
-    if ATTRS_TO_WATCH.any? { |k| changed_attributes.key?(k) }
-      FollowService.follow(self, updated_by, 'contributed')
-    end
+    return unless ATTRS_TO_WATCH.any? { |k| changed_attributes.key?(k) }
+
+    FollowService.follow(self, updated_by, 'contributed')
   end
   handle_asynchronously :after_updated_async
 
